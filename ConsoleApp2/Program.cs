e588c5f597b7b71e02becd9fdd26755e247e350d
Woohyuk Yang <ywh3589@naver.com>
2020-08-27 16:11:09 +0900

    초기 커밋.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Globalization;
+using Microsoft.VisualBasic.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ConsoleApp2
+{
+
+    class Product
+    {
+        private int price = 100;
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+
+        public void PrintPrice()
+        {
+            Console.WriteLine($"Price :{price}");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product carrot = new Product();
+            ref int ref_local_price = ref carrot.GetPrice();
+            int normal_local_price = carrot.GetPrice();
+
+            carrot.PrintPrice();
+            Console.WriteLine($"rEF lOCAL pRICE : {ref_local_price}");
+            Console.WriteLine($"Normal Local Price : {normal_local_price}");
+
+            ref_local_price = 200;
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal Local PrI: {normal_local_price}");
+
+
+
+
+
+        }
+    }
+}
